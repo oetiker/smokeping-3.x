6abdc54d5030e9837b435855084d3e13aa73ab51
Tobias Oetiker <tobi@oetiker.ch>
2013-09-24 18:34:47 +0200

    some more dependencies added

--- setup/build-thirdparty.sh
+++ setup/build-thirdparty.sh
@@ -7,11 +7,14 @@ for module in \
     MojoX::Dispatcher::Qooxdoo::Jsonrpc@0.93 \
     Mojo::Server::FastCGI@0.41 \
     Config::Grammar@1.10 \
+    Mail::Sender \
     Net::Telnet \
     Net::OpenSSH \
     Net::SNMP \
     Net::LDAP \
     Net::DNS \
+    Net::SNPP \
+    Mail::Sender \
     IO::Pty \
 ; do
     perlmodule $module
